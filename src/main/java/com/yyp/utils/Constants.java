0e45d31799dab89f0e2fa25240a44240dd6ba9ef
WarmWind <953450733@qq.com>
2022-05-21 09:58:14 +0800

    准备阶段

---
+++ src/main/java/com/yyp/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.yyp.utils;
+
+public class Constants {
+
+    public static final String TAG = "method";
+    public static final String FORWARD = "forward:";
+    public static final String REDIRECT = "redirect:";
+    public static final String FLAG = ":";
+    public static final String INDEX = "index";
+}
